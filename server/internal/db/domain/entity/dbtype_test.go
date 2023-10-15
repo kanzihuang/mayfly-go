2bd340362fa1f05948f0214beee365603c95b301
kanzihuang <kanzihuang@qq.com>
2023-10-15 10:36:01 +0800

    refactor: 实现 DbType 类型，集中处理部分差异化的数据库操作

--- server/internal/db/domain/entity/dbtype_test.go
+++ server/internal/db/domain/entity/dbtype_test.go
@@ -1,54 +1,50 @@
-package api
+package entity
 
 import (
 	"github.com/stretchr/testify/require"
-	"mayfly-go/internal/db/domain/entity"
 	"testing"
 )
 
-func Test_escapeSql(t *testing.T) {
+func Test_QuoteLiteral(t *testing.T) {
 	tests := []struct {
-		name   string
-		dbType string
+		dbType DbType
 		sql    string
 		want   string
 	}{
 		{
-			dbType: entity.DbTypeMysql,
+			dbType: DbTypeMysql,
 			sql:    "\\a\\b",
 			want:   "'\\\\a\\\\b'",
 		},
 		{
-			dbType: entity.DbTypeMysql,
+			dbType: DbTypeMysql,
 			sql:    "'a'",
 			want:   "'''a'''",
 		},
 		{
-			name:   "不间断空格",
-			dbType: entity.DbTypeMysql,
+			dbType: DbTypeMysql,
 			sql:    "a\u00A0b",
 			want:   "'a\u00A0b'",
 		},
 		{
-			dbType: entity.DbTypePostgres,
+			dbType: DbTypePostgres,
 			sql:    "\\a\\b",
 			want:   " E'\\\\a\\\\b'",
 		},
 		{
-			dbType: entity.DbTypePostgres,
+			dbType: DbTypePostgres,
 			sql:    "'a'",
 			want:   "'''a'''",
 		},
 		{
-			name:   "不间断空格",
-			dbType: entity.DbTypePostgres,
+			dbType: DbTypePostgres,
 			sql:    "a\u00A0b",
 			want:   "'a\u00A0b'",
 		},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got := escapeSql(tt.dbType, tt.sql)
+		t.Run(string(tt.dbType)+"_"+tt.sql, func(t *testing.T) {
+			got := tt.dbType.QuoteLiteral(tt.sql)
 			require.Equal(t, tt.want, got)
 		})
 	}
